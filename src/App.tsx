aaa08da55969d91db05aeb5d5126adedf5b5c001
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-11-06 19:25:00 +0530

    layout and api changes

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3333/api/short', {
+      const response = await fetch('https://petite.onrender.com/api/short', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -46,25 +46,39 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>URL Shortener</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Original URL:
-          <input type="text" value={origUrl} onChange={handleInputChange} />
-        </label>
-        <button type="submit">Shorten URL</button>
-      </form>
-      {loading && <div>Loading...</div>}
-      {shortUrl && (
-        <div>
-          Shortened URL:
-          <a href={shortUrl} target="_blank" rel="noopener noreferrer">
-            {shortUrl}
-          </a>
-        </div>
-      )}
-    </div>
+    <div className="flex flex-col justify-center items-center text-white">
+    <h1 className="text-4xl md:text-7xl text-center mb-4">URL Shortener</h1>
+    <form onSubmit={handleSubmit} className="w-full max-sm:w-2/3 max-w-sm">
+      <div className="flex flex-col mb-4 pt-10">
+        <input
+          type="text"
+          id="original-url"
+          value={origUrl}
+          onChange={handleInputChange}
+          placeholder="Enter the URL"
+          className="mt-1 p-2 rounded border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
+        />
+      </div>
+      <button type="submit" className="btn btn-success w-full py-2">Shorten URL</button>
+    </form>
+    {loading && <div className="mt-4 text-lg">Loading...</div>}
+    {shortUrl && (
+     <div className="mt-4 flex flex-col md:flex-row md:items-center">
+     <p className="text-lg font-medium">Shortened URL:</p>
+     <a href={shortUrl} target="_blank" rel="noopener noreferrer" className="text-gray-800 underline mt-2 md:mt-0 md:ml-2">
+       {shortUrl}
+     </a>
+     <button
+       onClick={() => navigator.clipboard.writeText(shortUrl)}
+       className="btn btn-secondary mt-2 md:mt-0 md:ml-2 flex items-center justify-center p-1"
+     >  Copy
+     </button>
+   </div>
+   
+    
+    )}
+  </div>
+  
   );
 };
 
