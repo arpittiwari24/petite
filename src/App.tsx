86d850e4c9ee4f9bd6780f0333722aef44b289ba
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-11-10 21:53:33 +0530

    added rate limiting features

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App: React.FC = () => {
   const [origUrl, setOrigUrl] = useState<string>('');
   const [shortUrl, setShortUrl] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false)
+  const [rateLimitExceeded, setRateLimitExceeded] = useState<boolean>(false);
+const [rateLimitMessage, setRateLimitMessage] = useState<string>('');
+
 
   const isValidUrl = (url: string) => {
     const pattern = /^(http|https):\/\/[^ "]+$/;
@@ -16,29 +19,36 @@ const App: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    setLoading(true)
-
+    setLoading(true);
+  
     if (!isValidUrl(origUrl)) {
       console.error('Invalid URL format');
+      setLoading(false);
       return;
     }
-
+  
     try {
-      const response = await fetch('https://petite.onrender.com/api/short', {
+      const response = await fetch('http://localhost:3333/api/short', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ origUrl }),
       });
-
-      if (response.ok) {
+  
+      if (response.status === 429) {
+        // Rate limit exceeded
+        const rateLimitMessage = await response.text();
+      setRateLimitExceeded(true);
+      setRateLimitMessage(rateLimitMessage);
+        setLoading(false)
+      } else if (response.ok) {
         const data = await response.json();
         setShortUrl(data.shortUrl);
-        setLoading(false)
+        setLoading(false);
       } else {
         console.error('Failed to shorten URL');
-        setLoading(false)
+        setLoading(false);
       }
     } catch (error) {
       console.error('Error:', error);
@@ -49,23 +59,33 @@ const App: React.FC = () => {
     <div className="flex flex-col justify-center items-center text-white">
     <h1 className="text-4xl md:text-7xl text-center mb-4">URL Shortener</h1>
     <form onSubmit={handleSubmit} className="w-full max-sm:w-2/3 max-w-sm">
-      <div className="flex flex-col mb-4 pt-10">
-        <input
-          type="text"
-          id="original-url"
-          value={origUrl}
-          onChange={handleInputChange}
-          placeholder="Enter the URL"
-          className="mt-1 p-2 rounded border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
-        />
-      </div>
-      <button type="submit" className="btn btn-success w-full py-2">Shorten URL</button>
-    </form>
+    <div className="flex flex-col mb-4 pt-10">
+      <input
+        type="text"
+        id="original-url"
+        value={origUrl}
+        onChange={handleInputChange}
+        placeholder="Enter the URL"
+        className="mt-1 p-2 rounded border border-gray-300 focus:ring focus:ring-blue-300 focus:outline-none"
+      />
+    </div>
+    <button type="submit" className="btn btn-success w-full py-2">Shorten URL</button>
+  </form>
     {loading && <div className="mt-4 text-lg">Loading...</div>}
+    {rateLimitExceeded && (
+      <>
+     <div className="mt-4 max-sm:text-lg lg:text-xl">
+       {rateLimitMessage}
+     </div>
+     <div className=' max-sm:w-72'>
+      <img src="https://media.tenor.com/JZJ7ukQTO24AAAAC/come-back-tomorrow-were-closed.gif" alt="Come back Tomorrow" />
+     </div>
+     </>
+    )}
     {shortUrl && (
      <div className="mt-4 flex flex-col md:flex-row md:items-center">
      <p className="text-lg font-medium">Shortened URL:</p>
-     <a href={shortUrl} target="_blank" rel="noopener noreferrer" className="text-gray-800 underline mt-2 md:mt-0 md:ml-2">
+     <a href={shortUrl} target="_blank" rel="noopener noreferrer" className="text-gray-800 bg-slate-300 underline mt-2 md:mt-0 md:ml-2">
        {shortUrl}
      </a>
      <button
